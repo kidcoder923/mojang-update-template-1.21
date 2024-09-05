1061e4951c4f3280b8fb89338be0f30cdb0a7d03
kidcoder923 <sciencguy70@gmail.com>
2024-09-05 04:45:27 -0600

    basic system

---
+++ src/main/java/net/strikezone/mupdate/mixin/MojangUpdateClient.java
@@ -0,0 +1,10 @@
+package net.strikezone.mupdate.mixin;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MojangUpdateClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
